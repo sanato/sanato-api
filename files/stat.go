ede4ec86e604935dea6e3308477196e67fca05ce
Hugo González Labrador <hglavra@gmail.com>
2015-03-23 22:23:29 +0100

    API Files stat

--- files/stat.go
+++ files/stat.go
@@ -1,82 +1,56 @@
 package files
 
 import (
-	"blacksync/lib/blacksync/storage"
-	"blacksync/lib/blacksync/auth"
-	"blacksync/lib/blacksync/config"
-	"blacksync/lib/blacksync/storage/storageproxy"
 	"encoding/json"
+	"github.com/Sirupsen/logrus"
+	"github.com/julienschmidt/httprouter"
+	"github.com/sanato/sanato-lib/storage"
 	"net/http"
-	"path/filepath"
 	"strconv"
-	"strings"
-
-	log "github.com/Sirupsen/logrus"
 )
 
-func Stat() http.Handler {
-	fn := func(w http.ResponseWriter, r *http.Request) {
-
-		log.WithFields(log.Fields{
-			"frontend": "api",
-			"app":      "files",
-			"action":   "stat",
-		}).Debug("files app stat resource")
-
-		cfg, err := config.Get()
-		if err != nil {
-			log.Error(err)
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-			return
-		}
-
-		authRes := auth.GetAuthResourceFromRequest(r)
-
-		storageProxy, err := storageproxy.NewStorageProxy(cfg.Storage, authRes)
-		if err != nil {
-			log.Error(err)
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-			return
-		}
-
-		var children bool
-		queryChildren := r.URL.Query().Get("children")
-		if queryChildren != "" {
-			children, err = strconv.ParseBool(queryChildren)
-			if err != nil {
-				children = false
-			}
-		}
-
-		if err != nil {
-			log.Warn(err)
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		
-		resource := filepath.Join("/", strings.TrimPrefix(r.URL.Path, "/api/v1/core/apps/files/stat"))
-		
-		meta, err := storageProxy.Stat(resource, children)
-		if err != nil {
-			if storage.IsNotExistError(err){
-				log.Error(err)
-				http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-				return
-			}
-			log.Error(err)
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-			return
-		}
-
-		metaJSON, err := json.Marshal(meta)
-		if err != nil {
-			log.Error(err)
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-			return
-		}
-		w.WriteHeader(http.StatusOK)
-		w.Write(metaJSON)
+func (api *API) stat(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	_, err := api.tokenAuth(r, true)
+	if err != nil {
+		logrus.Error(err)
+		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
 		return
 	}
-	return http.HandlerFunc(fn)
+	resource := p.ByName("path")
+	if resource == "" {
+		resource = "/"
+	}
+	var children bool
+	queryChildren := r.URL.Query().Get("children")
+	if queryChildren != "" {
+		children, err = strconv.ParseBool(queryChildren)
+		if err != nil {
+			children = false
+		}
+	}
+	if err != nil {
+		logrus.Warn(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	meta, err := api.storageProvider.Stat(resource, children)
+	if err != nil {
+		if storage.IsNotExistError(err) {
+			logrus.Error(err)
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+			return
+		}
+		logrus.Error(err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	metaJSON, err := json.Marshal(meta)
+	if err != nil {
+		logrus.Error(err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(metaJSON)
+	return
 }
