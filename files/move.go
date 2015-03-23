6a1a5e6cd2ef8a3ae23e20cbd95ec0003cf1e21b
Hugo González Labrador <hglavra@gmail.com>
2015-03-23 22:20:40 +0100

    API Files move

---
+++ files/move.go
@@ -0,0 +1,33 @@
+package files
+
+import (
+	"github.com/Sirupsen/logrus"
+	"github.com/julienschmidt/httprouter"
+	"github.com/sanato/sanato-lib/storage"
+	"net/http"
+	"path/filepath"
+)
+
+func (api *API) move(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	_, err := api.tokenAuth(r, true)
+	if err != nil {
+		logrus.Error(err)
+		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+		return
+	}
+	from := filepath.Clean(r.URL.Query().Get("from"))
+	to := filepath.Clean(r.URL.Query().Get("to"))
+	err = api.storageProvider.Rename(from, to)
+	if err != nil {
+		if storage.IsNotExistError(err) {
+			logrus.Warn(err)
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+			return
+		}
+		logrus.Error(err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	return
+}
