c2a2aa2dfdd55bbccbb64567e2983c2455db74e9
Hugo González Labrador <hglavra@gmail.com>
2015-03-23 22:19:25 +0100

    Files API delete

---
+++ files/delete.go
@@ -0,0 +1,42 @@
+package files
+
+import (
+	"github.com/Sirupsen/logrus"
+	"github.com/julienschmidt/httprouter"
+	"github.com/sanato/sanato-lib/storage"
+	"net/http"
+)
+
+func (api *API) delete(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	_, err := api.tokenAuth(r, true)
+	if err != nil {
+		logrus.Error(err)
+		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+		return
+	}
+	resource := p.ByName("path")
+	if resource == "" {
+		resource = "/"
+	}
+	_, err = api.storageProvider.Stat(resource, false)
+	if err != nil { //DELETE on null resource gave 500, should be 404 (RFC2518:S3)
+		if storage.IsNotExistError(err) {
+			logrus.Warn(err)
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+			return
+		}
+		logrus.Error(err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	err = api.storageProvider.Remove(resource, true)
+	if err != nil {
+		logrus.Error(err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+	return
+}
